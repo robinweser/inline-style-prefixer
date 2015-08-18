69d9b43dcbb12603cb549a60a514fdcbe5fd463f
rofrischmann <robin@rofrischmann.de>
2015-08-18 13:37:08 +0200

    #1 added a bunch of new properties that need prefixes

--- generator/generatePrefixData.js
+++ generator/generatePrefixData.js
@@ -1,4 +1,4 @@
-let canIUse = require('caniuse-api');
+let caniuse = require('caniuse-api');
 import searchMap from './searchMap';
 import fs from 'fs';
 
@@ -15,7 +15,7 @@ function gatherInformation() {
 	let search;
 	for (search in searchMap) {
 		let properties = searchMap[search];
-		let versions = canIUse.getSupport(search, true);
+		let versions = caniuse.getSupport(search, true);
 		if (properties instanceof Array !== true) {
 			properties = [properties];
 		}
@@ -33,4 +33,4 @@ function gatherInformation() {
 fs.writeFile('./lib/data.js', gatherInformation(), err => {
 	if (err) throw err;
 	console.log("Successfully generated prefix data.");
-})
+});
