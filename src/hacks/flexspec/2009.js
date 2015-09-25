2355751b52a0bd2198eb8e624cb6cb2473942d76
rofrischmann <robin@rofrischmann.de>
2015-09-25 23:18:55 +0200

    added support for android flexbox specs

--- src/hacks/flexspec/2009.js
+++ src/hacks/flexspec/2009.js
@@ -1,16 +1,51 @@
-function condition(browserInfo){
-	return browserInfo.prefix.inline == 'Webkit' && browserInfo.android &&  browserInfo.version < 4.4;
+function condition(browserInfo) {
+  return browserInfo.android && browserInfo.version < 4.4
 }
 
 export default function hack(browserInfo) {
-	if (condition(browserInfo)){
+  if (condition(browserInfo)) {
 
-		return {
-			prefixValue: {},
-			alternativeProperty : {},
-			alternativeValue: {}
-		}
-	} else {
-		return false;
-	}
+    let alignValues = {
+      'space-around': 'justify',
+      'space-between': 'justify',
+      'flex-start': 'start',
+      'flex-end': 'end'
+    }
+
+    return {
+      alternativeProperty: {
+        alignItems: 'WebkitBoxAlign',
+        justifyContent: 'WebkitBoxPack',
+        flexWrap: 'WebkitBoxLines',
+        flexDirection: ['WebkitBoxOrient', 'WWebkitBoxDirection']
+      },
+      alternativeValue: {
+        justifyContent: alignValues,
+        alignContent: alignValues,
+        WebkitBoxLines: {
+          'wrap-reverse': 'multiple',
+          'wrap': 'multiple'
+        },
+        WebkitBoxOrient: {
+          'column': 'vertical',
+          'column-reverse': 'vertical',
+          'row': 'horizontal',
+          'row-reverse': 'horizontal'
+        },
+        WebkitBoxDirection: {
+          'column': 'normal',
+          'column-reverse': 'reverse',
+          'row': 'normal',
+          'row-reverse': 'reverse'
+        },
+        display: {
+          'flex': browserInfo.prefix.CSS + 'box',
+          'inline-flex': browserInfo.prefix.CSS + 'inline-box'
+        }
+      }
+    }
+  } else {
+    return false
+  }
 }
+
