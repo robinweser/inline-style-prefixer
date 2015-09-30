ed747b7c03ec81af2066e6d14f448f950505c18a
rofrischmann <robin@rofrischmann.de>
2015-09-30 22:51:00 +0200

    #6 added calc Plugin
    this closes #6

---
+++ src/plugins/calc.js
@@ -0,0 +1,7 @@
+export default (property, value, {browser, version, prefix}) => {
+  if (value.indexOf('calc(') > -1 && browser === 'firefox' && version < 15 || browser === 'chrome' && version < 25 || browser === 'safari' && version < 6.1 || browser === 'ios_saf' && version < 7) {
+    return {
+      [property]: value.replace(/calc\(/g, prefix.CSS + 'calc(')
+    }
+  }
+}
