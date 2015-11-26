b4957fc347dca430c632dec5f8c7fead7f6da612
rofrischmann <robin@rofrischmann.de>
2015-11-26 18:38:21 +0100

    #28 added simple test for 2D transforms

--- test/prefixer-test.js
+++ test/prefixer-test.js
@@ -1,13 +1,14 @@
 import { expect } from 'chai'
 import Prefixer from '../lib/Prefixer'
 
-let MSIE10 = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'
-let MSIE11 = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
+const MSIE9 = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)'
+const MSIE10 = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)'
+const MSIE11 = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
 
-let Android4_4_4 = 'Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36'
-let Chrome14 = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.812.0 Safari/535.1'
-let Chrome45 = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
-let Chrome49 = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2454.85 Safari/537.36'
+const Android4_4_4 = 'Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Mobile Safari/537.36'
+const Chrome14 = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.812.0 Safari/535.1'
+const Chrome45 = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
+const Chrome49 = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2454.85 Safari/537.36'
 
 
 describe('Prefixing a property', () => {
@@ -17,6 +18,19 @@ describe('Prefixing a property', () => {
     let prefixed = {WebkitAppearance: 'test', transition: 'test'}
     expect(new Prefixer(Chrome45).prefix(input)).to.eql(prefixed)
     expect(new Prefixer(Chrome49).prefix(input2)).to.eql(input2)
+  })
+})
+
+describe('Prefixing 2D transforms', () => {
+  let input = {transform: 'rotate(30deg)'}
+  let prefixed = {msTransform: 'rotate(30deg)'}
+
+  it('should be prefixed on IE 9', () => {
+    expect(new Prefixer(MSIE9).prefix(input)).to.eql(prefixed)
+  })
+
+  it('should not be prefixed on IE 10', () => {
+    expect(new Prefixer(MSIE10).prefix(input)).to.eql(input)
   })
 })
 
