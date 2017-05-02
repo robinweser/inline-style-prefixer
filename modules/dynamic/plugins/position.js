0c3b83be90053fbaba05f855d88586f4db36482d
rofrischmann <robin@rofrischmann.de>
2017-05-02 21:19:27 +0200

    #126 fix position plugin
    closes #126

--- modules/dynamic/plugins/position.js
+++ modules/dynamic/plugins/position.js
@@ -6,12 +6,13 @@ import type { PluginMetaData } from '../../../flowtypes/PluginMetaData'
 export default function position(
   property: string,
   value: any,
+  style: Object,
   { browserName, cssPrefix, keepUnprefixed }: PluginMetaData
 ): ?Array<any> | ?any {
   if (
     property === 'position' &&
-      value === 'sticky' &&
-      (browserName === 'safari' || browserName === 'ios_saf')
+    value === 'sticky' &&
+    (browserName === 'safari' || browserName === 'ios_saf')
   ) {
     return getPrefixedValue(cssPrefix + value, value, keepUnprefixed)
   }
